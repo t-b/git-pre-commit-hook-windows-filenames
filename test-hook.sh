c413d7c2bd23404d935209e4509285ca1f633095
Martin Unzner <martin.u@posteo.de>
2020-06-15 21:53:10 +0200

    add tests for updated regular expression

--- test-hook.sh
+++ test-hook.sh
@@ -17,13 +17,15 @@ checkname ()
   
   filename="$1"
   counter=$(expr $counter \+ 1)
+  mkdir -p "$(dirname "$filename")" &&
   touch "$filename" &&
   git add "$filename" &&
   GIT_TRACE=1 git commit -m "my message"
   ret=$?
   git reset --hard $commit > /dev/null
 
-  if test $ret -eq 1
+  expected_exit_code=${2:-1}
+  if test $ret -eq $expected_exit_code
   then
     echo "Test $counter passed"
   else
@@ -50,6 +52,10 @@ echo "###reserved names###"
 checkname "CON.txt" 
 checkname "AUX.txt" 
 checkname "LPT1.txt" 
+checkname "COM1"
+checkname "NUL"
+checkname "sub/folder/PRN"
+checkname "comic-con.txt" 0
 
 echo "###no trailing period or space###"
 checkname " "
